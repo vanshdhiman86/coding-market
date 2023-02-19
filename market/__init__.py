f26fc6680fb85b9808c6ef3f7b485b8182e2dac2
Vansh Dhiman <vanshdhiman86@gmail.com>
2023-02-20 00:56:22 +0530

    add DATABASE

--- market/__init__.py
+++ market/__init__.py
@@ -4,9 +4,9 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://market_diba_user:v4Q2zWoojCkdLyy82C3piI01kN8jLDNR@dpg-cfor28g2i3mo4buptv2g-a.oregon-postgres.render.com/market_diba'
-# 'sqlite:///market.db'
-# postgresql://market_diba_user:v4Q2zWoojCkdLyy82C3piI01kN8jLDNR@dpg-cfor28g2i3mo4buptv2g-a.oregon-postgres.render.com/market_diba
+DATABASE_URL = 'postgresql://market_diba_user:v4Q2zWoojCkdLyy82C3piI01kN8jLDNR@dpg-cfor28g2i3mo4buptv2g-a.oregon-postgres.render.com/market_diba'
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+
 app.config['SECRET_KEY'] = '826a8b27acf76da67281a678'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
